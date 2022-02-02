7a366ed4fabc9b3ec78faade20a9ecac2d72161b
arvindh devil <95742852+Arvindhdevil@users.noreply.github.com>
2022-02-02 05:28:02 +0000

    3rd question

---
+++ p4original.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int input_n()
+{
+ int n;
+ printf("Enter the numbers\n");
+ scanf("%d",&n);
+ return n;
+}
+int sum_n(int n)
+{
+ int sum =0,i;
+ for(i=1;i<=n;i++)
+ {
+  sum = sum + i;
+ }
+ return sum;
+}
+void output(int n, int sum)
+{
+   int i ;
+   for( i=1;i<n;i++)
+ {
+  printf("%d+",i);
+ }
+  printf("%d = %d",n,sum);
+}
+int main()
+{
+  int a,b;
+  a = input_n();
+  b = sum_n(a);
+  output(a,b);
+}
